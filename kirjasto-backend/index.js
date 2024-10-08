13e5632b46fee1f80727357168b50ed51120ee41
Smiboi <128560300+Smiboi@users.noreply.github.com>
2024-10-08 12:15:52 +0300

    Tehtävä 8.6 tehty

--- kirjasto-backend/index.js
+++ kirjasto-backend/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
+const { v1: uuid } = require('uuid')
 
 let authors = [
   {
@@ -109,6 +110,15 @@ const typeDefs = `
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
+
+  type Mutation {
+    addBook(
+      title: String!
+      published: Int!
+      author: String!
+      genres: [String!]!
+    ): Book
+  }
 `
 
 const resolvers = {
@@ -133,6 +143,17 @@ const resolvers = {
         }
       }
       return bookCount
+    }
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      if (!authors.find(p => p.name === args.author)) {
+        const author = { name: args.author, id: uuid() }
+        authors = authors.concat(author)
+      }
+      const book = { ...args, id: uuid() }
+      books = books.concat(book)
+      return book
     }
   }
 }
